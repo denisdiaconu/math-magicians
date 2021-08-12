8433e927f0530e0f7d88eff62240e3751f30615a
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-08-12 11:52:59 +0200

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,5 @@
-/* eslint-disable */
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
 export default class Calculator extends Component {
   constructor(props) {
@@ -13,6 +11,7 @@ export default class Calculator extends Component {
       operation: '',
     };
   }
+
   handleState(newState) {
     if (newState.total != null) {
       this.setState({ total: newState.total });
@@ -25,50 +24,53 @@ export default class Calculator extends Component {
     }
   }
 
-  display(next) {
+  getInf(state, buttonName) {
+    this.handleState(calculate(state, buttonName));
+  }
+
+  display = (next) => {
     if (next != null) {
       return next;
-    } else {
-      next = ''
-      return next;
     }
+    const newnext = '';
+    return newnext;
   }
 
   render() {
     const { total, next, operation } = this.state;
     return (
       <div className="calContainer">
-        <input className="calInput" value={total + ' ' + operation + this.display(next)} disabled="disabled" />
+        <input className="calInput" value={`${total} ${operation}${this.display(next)}`} disabled="disabled" />
         <div className="calNumbers">
           <div className="conFirst">
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, 'AC')); }}>AC</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '+/-')); }}>+/-</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '%')); }}>%</button>
-            <button className="colorS" type="button" onClick={() => { this.handleState(calculate(this.state, '÷')); }}>÷</button>
+            <button className="colorW" type="button" name="AC" onClick={(e) => { this.getInf(this.state, e.target.name); }}>AC</button>
+            <button className="colorW" type="button" name="+/-" onClick={(e) => { this.getInf(this.state, e.target.name); }}>+/-</button>
+            <button className="colorW" type="button" name="%" onClick={(e) => { this.getInf(this.state, e.target.name); }}>%</button>
+            <button className="colorS" type="button" name="÷" onClick={(e) => { this.getInf(this.state, e.target.name); }}>÷</button>
           </div>
           <div className="conSecond">
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '7')); }}>7</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '8')); }}>8</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '9')); }}>9</button>
-            <button className="colorS" type="button" onClick={() => { this.handleState(calculate(this.state, 'x')); }}>x</button>
+            <button className="colorW" type="button" name="7" onClick={(e) => { this.getInf(this.state, e.target.name); }}>7</button>
+            <button className="colorW" type="button" name="8" onClick={(e) => { this.getInf(this.state, e.target.name); }}>8</button>
+            <button className="colorW" type="button" name="9" onClick={(e) => { this.getInf(this.state, e.target.name); }}>9</button>
+            <button className="colorS" type="button" name="x" onClick={(e) => { this.getInf(this.state, e.target.name); }}>x</button>
           </div>
           <div className="conThird">
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '4')); }}>4</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '5')); }}>5</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '6')); }}>6</button>                
-            <button className="colorS" type="button" onClick={() => { this.handleState(calculate(this.state, '-')); }}>-</button> 
+            <button className="colorW" type="button" name="4" onClick={(e) => { this.getInf(this.state, e.target.name); }}>4</button>
+            <button className="colorW" type="button" name="5" onClick={(e) => { this.getInf(this.state, e.target.name); }}>5</button>
+            <button className="colorW" type="button" name="6" onClick={(e) => { this.getInf(this.state, e.target.name); }}>6</button>
+            <button className="colorS" type="button" name="-" onClick={(e) => { this.getInf(this.state, e.target.name); }}>-</button>
           </div>
           <div className="conFourth">
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '1')); }}>1</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '2')); }}>2</button>
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '3')); }}>3</button>
-            <button className="colorS" type="button" onClick={() => { this.handleState(calculate(this.state, '+')); }}>+</button>
+            <button className="colorW" type="button" name="1" onClick={(e) => { this.getInf(this.state, e.target.name); }}>1</button>
+            <button className="colorW" type="button" name="2" onClick={(e) => { this.getInf(this.state, e.target.name); }}>2</button>
+            <button className="colorW" type="button" name="3" onClick={(e) => { this.getInf(this.state, e.target.name); }}>3</button>
+            <button className="colorS" type="button" name="+" onClick={(e) => { this.getInf(this.state, e.target.name); }}>+</button>
           </div>
           <div className="conFifth">
-            <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '0')); }}>0</button>
+            <button className="colorW" type="button" name="0" onClick={(e) => { this.getInf(this.state, e.target.name); }}>0</button>
             <div>
-              <button className="colorW" type="button" onClick={() => { this.handleState(calculate(this.state, '.')); }}>.</button>
-              <button className="colorS" type="button" onClick={() => { this.handleState(calculate(this.state, '=')); }}>=</button>
+              <button className="colorW" type="button" name="." onClick={(e) => { this.getInf(this.state, e.target.name); }}>.</button>
+              <button className="colorS" type="button" name="=" onClick={(e) => { this.getInf(this.state, e.target.name); }}>=</button>
             </div>
           </div>
         </div>

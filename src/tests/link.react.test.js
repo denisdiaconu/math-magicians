87f498bac13831e08490c118b0738fe9ad22c6eb
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-08-17 12:01:17 +0200

    Fix linters errors

--- src/tests/link.react.test.js
+++ src/tests/link.react.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-
 it('Render works correctly', () => {
-    const tree= renderer.create(<div>buttons</div>).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+  const tree = renderer.create(<div>buttons</div>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
